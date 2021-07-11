8118c40f4d41b5f777a0061d9e52e1ec3a3dc44a
jariryyousef <jariryyousef@gmail.com>
2021-07-11 14:27:13 +0300

    test

---
+++ javas/app.js
@@ -0,0 +1,19 @@
+'use strict';
+let hours =['6am','7am','8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+
+let location1={
+name: "Seattle",
+min:23,
+max:65,
+avg:6.3,
+
+
+random=function(min,max){
+    return Math.floor(Math.random() * (max - min + 1) ) + min
+    console.log(random)
+},
+
+
+
+
+}
